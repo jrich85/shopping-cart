92e8f03a95cd41334888a9c997c81af28fc422a1
Joey Richard <joey.richard@gmail.com>
2023-08-28 13:34:01 -0300

    Missed two components that have been in development.

---
+++ resources/js/components/ShoppingLists.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container-fluid application">
+        <div class="application">
+            <v-card>
+                <v-card-title> Shopping Lists </v-card-title>
+
+                <v-card-text>
+                    <shopping-lists-list :lists="lists" />
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-text-field
+                        label="Give your new list a title"
+                        v-model="newListTitle"
+                        v-on:keyup.enter="newList"
+                        :disabled="disableNewList"
+                        :error-messages="newListErrors"
+                        hint="Must be a unique title"
+                    />
+                    <v-btn @click="newList">Create New List</v-btn>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.application {
+    height: 100vh;
+}
+</style>
+
+<script setup>
+import api from "../api/api";
+import ShoppingListsList from "../components/ShoppingListsList.vue";
+import { computed, onMounted, ref, reactive } from "vue";
+
+onMounted(() => {
+    getLists();
+});
+
+// region dataonMounted
+let lists = reactive([]);
+let newListTitle = ref("");
+let newListErrors = reactive([]);
+// endregion data
+
+// region computed vars
+const disableNewList = computed(
+    () => newListTitle !== "" && newListErrors === []
+);
+// region computed vars
+
+// region api actions
+const getLists = () =>
+    api.getShoppingLists().then((sourceLists) => {
+        lists.length = 0;
+        if (sourceLists.data.length) {
+            sourceLists.data.forEach((list) => {
+                lists.push(list);
+            });
+        }
+    });
+
+const newList = () => {
+    newListErrors.length = 0;
+    if (newListTitle.value.trim() === "") {
+        newListErrors.push("Required field");
+        return;
+    }
+    api.createShoppingList(newListTitle.value).then((newList) => {
+        if (newList.errors) {
+            const errorToShow = newList.errors.name;
+            Object.keys(errorToShow).forEach((key) =>
+                newListErrors.push(errorToShow[key])
+            );
+        } else {
+            newListTitle = "";
+            getLists();
+        }
+    });
+};
+// endregion api actions
+</script>
---
+++ resources/js/components/ShoppingListsList.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-list>
+        <v-list-item
+            v-for="list in lists"
+            :key="list.id"
+            :title="list.name"
+            :subtitle="`created ${list.created_at}`"
+            @click="$router.push(`/lists/${list.id}`)"
+        />
+    </v-list>
+
+</template>
+
+<script setup>
+
+
+const props = defineProps(['lists']);
+
+
+</script>
