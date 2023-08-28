e4eb68fea5bd507faab231d96cf925c4dcd3044d
Joey Richard <joey.richard@gmail.com>
2023-08-28 11:22:31 -0300

    Shoring up the api actions for retrieving all, and creating a new, shopping list, with error messaging.

--- resources/js/api/api.js
+++ resources/js/api/api.js
@@ -1,22 +1,32 @@
 const api = {
     getShoppingLists: async () => {
         try {
-            const lists = await window.axios.get('api/grocery-list');
+            const lists = await window.axios.get("api/grocery-list");
 
-            return lists;
+            return lists.data;
         } catch (error) {
-            console.error(error);
-        }
+            const messages = error.response.data.errors;
 
-        return
+            return {
+                errors: messages
+            };
+        }
     },
     createShoppingList: async (name) => {
         try {
-            const newList = await window.axios.post('api/grocery-list', {name: name});
+            const newList = await window.axios.post("api/grocery-list", {
+                name: name,
+            });
+
+            return newList.data;
         } catch (error) {
-            console.error(error);
+            const messages = error.response.data.errors;
+
+            return {
+                errors: messages
+            };
         }
-    }
+    },
 };
 
 export default api;
--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,27 +1,77 @@
 <template>
-    <div class="container-fluid">
-        <div class="card">
-            <div class="card-header">Shopping Lists</div>
+    <div class="container-fluid h-100">
+        <v-card>
+            <v-card-title> Shopping Lists </v-card-title>
 
-            <div class="card-body">
-                Are there lists? {{ lists.length? 'yes' : 'no' }}
-            </div>
+            <v-card-text>
+                Are there lists? {{ lists.length ? "yes" : "no" }}
+            </v-card-text>
 
-            <button @click="getLists">click to get lists</button>
-        </div>
+            <v-card-actions>
+                <v-text-field
+                    label="Give your new list a title"
+                    v-model="newListTitle"
+                    v-on:keyup.enter="newList"
+                    :disabled="disableNewList"
+                    :error-messages="newListErrors"
+                    hint="Must be a unique title"
+                />
+                <v-btn @click="newList">Create New List</v-btn>
+            </v-card-actions>
+        </v-card>
+
+        {{ newListErrors }}
+        {{ newListTitle }}
     </div>
 </template>
 
 <script setup>
-import api from '../api/api'
-import { onMounted } from 'vue'
-
-let lists = Array();
+import api from "../api/api";
+import { computed, onMounted, ref, reactive } from "vue";
 
 onMounted(() => {
-    // lists = api.getShoppingLists();
-})
+    getLists();
+});
 
-const getLists = () => lists = api.getShoppingLists();
+// region data
+let lists = reactive([]);
+let newListTitle = ref("");
+let newListErrors = reactive([]);
+// endregion data
 
+// region computed vars
+const disableNewList = computed(
+    () => newListTitle !== "" && newListErrors === []
+);
+// region computed vars
+
+// region api actions
+const getLists = () =>
+    api.getShoppingLists().then((sourceLists) => {
+        lists.length = 0;
+        if (sourceLists.data.length) {
+            sourceLists.data.forEach((list) => {
+                lists.push(list);
+            });
+        }
+    });
+
+const newList = () => {
+    newListErrors.length = 0;
+    if (newListTitle.value.trim() === "") {
+        newListErrors.push();
+        return;
+    }
+    api.createShoppingList(newListTitle.value).then((newList) => {
+        if (newList.errors) {
+            const errorToShow = newList.errors.name;
+            Object.keys(errorToShow).forEach((key) =>
+                newListErrors.push(errorToShow[key])
+            );
+        } else {
+            getLists();
+        }
+    });
+};
+// endregion api actions
 </script>
