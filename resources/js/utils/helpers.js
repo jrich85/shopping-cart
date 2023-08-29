99195664244d1ad6461589b7d31088d5902e1148
Joey Richard <joey.richard@gmail.com>
2023-08-28 23:18:51 -0300

    Changing date format to use timeAgo instead.

--- resources/js/utils/helpers.js
+++ resources/js/utils/helpers.js
@@ -1,6 +1,14 @@
+import TimeAgo from 'javascript-time-ago'
+// English.
+import en from 'javascript-time-ago/locale/en'
+
 export const dateFormat = (dateString) => {
+    TimeAgo.addDefaultLocale(en)
     const date = new Date(dateString);
 
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    // Create formatter (English).
+    const timeAgo = new TimeAgo('en-US')
+
+    return timeAgo.format(date);
 };
 
